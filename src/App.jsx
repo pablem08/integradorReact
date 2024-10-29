7048d0c3bd0ecb227d0f318f51481ef9c42eca10
PabloRiveros <pablem08@gmail.com>
2024-10-29 16:48:39 -0300

    Armado y modelado de las carpetas del proyecto, para comenzar a crear las ramas

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-600">
+        Arrancando Integrador de react
+      </h1>
+    </>
+  );
+}
+
+export default App;
