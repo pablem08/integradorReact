efde7eb8b092108cb55ee436c56627d4a4d328a7
PabloRiveros <pablem08@gmail.com>
2024-10-31 16:21:02 -0300

    cree la rama navbar para empezar a desarrollar el mismo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-600">
-        Arrancando Integrador de react
-      </h1>
+      <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <>
+      <h1>Empezamos a crear la rama navbar</h1>
+    </>
+  );
+};
